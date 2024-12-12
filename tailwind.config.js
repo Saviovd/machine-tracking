ded74cc7e94a17f8670416524254e2f1d9fa63a8
Saviovd <savio.pinheiroal@gmail.com>
2024-12-12 15:09:29 -0400

    👌 IMPROVE: exporting tailwind configs in the correct way

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-module.exports = {
-   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+export default {
    content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
    theme: {
       extend: {
          screens: {
-            'xs': '480px',
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px',
+            xs: '480px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
             '2xl': '1536px',
          },
       },
